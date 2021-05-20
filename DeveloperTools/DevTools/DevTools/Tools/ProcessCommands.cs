6f7a58f6be0ab27919d36c42b8a9ed49131d87f4
Josh <joshvelasqez6@gmail.com>
2021-05-19 21:41:18 -0600

    Moved everything from .NETcore to .NET framework

---
+++ DeveloperTools/DevTools/DevTools/Tools/ProcessCommands.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace DevTools.Tools
+{
+    static class ProcessCommands
+    {
+        public static void RunCommand(string command)
+        {
+            try
+            {
+                Process.Start(command);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Failed to run command: " + command + "\nError: " + e);
+            }
+        }
+
+        public static void RunCommand(string command, string args)
+        {
+            try
+            {
+                Process.Start(command, args);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Failed to run command: " + command + "\nError: " + e);
+            }
+        }
+
+        public static void RunCommands(string terminal, string[] commands)
+        {
+            StringBuilder commandsPipe = new StringBuilder();
+            try
+            {
+                for (int i = 0; i < commands.Length - 1; i++)
+                {
+                    commandsPipe.Append(commands[i] + " && ");
+                }
+                commandsPipe.Append(commands[commands.Length - 1]);
+                Process.Start(terminal, commandsPipe.ToString());
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Failed to run commands.\nError: " + e);
+            }
+        }
+
+
+        public static void DeleteDirectory(string path)
+        {
+            try
+            {
+                if (Directory.Exists(path))
+                    Directory.Delete(path, true);
+                else
+                    throw new DirectoryNotFoundException();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Failed to delete directory: " + path + "\nError: " + e);
+            }
+        }
+    }
+}
---
+++ DeveloperTools/DevTools/DevTools/Tools/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace DevTools.Tools
+{
+    enum Programs
+    {
+        [Description("slack")]
+        Slack,
+        [Description("outlook")]
+        Outlook,
+        [Description("chrome")]
+        Chrome
+    }
+}
