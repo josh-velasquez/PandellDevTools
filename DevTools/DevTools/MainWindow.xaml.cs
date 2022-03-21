e860012065a9de839323083ec4c31ef7e8b9bbda
Josh Velasquez <joshv@pandell.com>
2022-03-21 14:04:05 -0600

    Update path

--- DevTools/DevTools/MainWindow.xaml.cs
+++ DevTools/DevTools/MainWindow.xaml.cs
@@ -14,13 +14,13 @@ namespace DevTools
     /// </summary>
     public partial class MainWindow : Window
     {
-        const string SLACK_PATH = @"C:\Users\joshv\AppData\Local\slack\slack.exe";
-        const string SPOTIFY_PATH = @"C:\Users\joshv\AppData\Roaming\Spotify\Spotify.exe";
-        const string WINDOWSTERMINAL_PATH = @"C:\Users\joshv\AppData\Local\Microsoft\WindowsApps\wt.exe";
-        const string TEMP_ASP_FILES = @"C:\Users\joshv\AppData\Local\Temp\Temporary ASP.NET Files";
-        const string OUTLOOK_PATH = @"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE";
-        const string CHROME_PATH = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
-        const string CONFIG_PATH = @"D:\Repository\LandRiteWeb\src\Pandell.LandRite.Web\Pli.config.devel.jsonc";
+        string SLACK_PATH = @"\Local\slack\slack.exe";
+        string SPOTIFY_PATH = @"\Roaming\Spotify\Spotify.exe";
+        string WINDOWSTERMINAL_PATH = @"\Local\Microsoft\WindowsApps\wt.exe";
+        string TEMP_ASP_FILES = @"\Local\Temp\Temporary ASP.NET Files";
+        string OUTLOOK_PATH = @"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE";
+        string CHROME_PATH = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+        string CONFIG_PATH = @"\src\Pandell.LandRite.Web\Pli.config.devel.jsonc";
         IDictionary<string, string> WEBSITES = new Dictionary<string, string>()
         {
             {"GitHub", "https://github.com/pandell/LandRiteWeb"},
@@ -30,38 +30,24 @@ namespace DevTools
             { "EmployeePortal", "http://employee/"}
         };
 
-        int HORIZONTAL_SCREEN_WIDTH = (int)SystemParameters.PrimaryScreenWidth;
-        int HORIZONTAL_SCREEN_HEIGHT = (int)SystemParameters.PrimaryScreenHeight;
-
         public MainWindow()
         {
             InitializeComponent();
+            UpdateUserPath();
+            UpdateConfigPath();
         }
 
-        private bool ConnectToVpn()
+        private void UpdateConfigPath()
         {
-            try
-            {
-                // wait for 2 seconds
-                Thread.Sleep(2000);
+            CONFIG_PATH = ProjectPath.Text + CONFIG_PATH;
+        }
 
-                // Move and to tray icon
-                MouseCommands.MoveAndClick(2351, (1440 - 16));
-                Thread.Sleep(500);
-
-                // Move and click to connect icon
-                MouseCommands.MoveAndClick(2401, (1440 - 77));
-                Thread.Sleep(10000);
-
-                // close tray
-                MouseCommands.MoveAndClick(2351, (1440 - 16));
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("Error: " + e);
-                return false;
-            }
-            return true;
+        private void UpdateUserPath()
+        {
+            SLACK_PATH = UserPath.Text + SLACK_PATH;
+            SPOTIFY_PATH = UserPath.Text + SPOTIFY_PATH;
+            WINDOWSTERMINAL_PATH = UserPath.Text + WINDOWSTERMINAL_PATH;
+            TEMP_ASP_FILES = UserPath.Text + TEMP_ASP_FILES;
         }
 
         private bool LaunchSlack()
@@ -172,104 +158,6 @@ namespace DevTools
             return false;
         }
 
-        private bool CloseSlack()
-        {
-            try
-            {
-                WindowCommands.CloseWindow(Programs.Slack);
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("Failed to close Slack: " + e);
-                return false;
-            }
-            return true;
-        }
-
-        private bool CloseOutlook()
-        {
-            try
-            {
-                WindowCommands.CloseWindow(Programs.Outlook);
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("Failed to close Outlook: " + e);
-                return false;
-            }
-            return true;
-        }
-
-        private bool CloseChrome()
-        {
-            try
-            {
-                WindowCommands.CloseWindow(Programs.Chrome);
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("Failed to close Chrome: " + e);
-                return false;
-            }
-            return true;
-        }
-
-        private bool CloseSpotify()
-        {
-            try
-            {
-                WindowCommands.CloseWindow(Programs.Spotify);
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("Failed to close Spotify: " + e);
-                return false;
-            }
-            return true;
-        }
-
-        private bool CloseWindowsTerminal()
-        {
-            try
-            {
-                WindowCommands.CloseWindow(Programs.WindowsTerminal);
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("Failed to close Windows Terminal: " + e);
-                return false;
-            }
-            return true;
-        }
-        private bool CheckTimeSheetSubmission()
-        {
-            var buttons = MessageBoxButton.YesNoCancel;
-            var result = MessageBox.Show("Make sure to submit your time sheet for approval! Did you submit already?", "Time Sheet Submission Reminder", buttons);
-            if (result == MessageBoxResult.Yes)
-                return true;
-            return false;
-        }
-
-        private bool ResizeWindowRightHalfTopVertical(Programs targetWindow)
-        {
-            return WindowCommands.ResizeWindowRightHalfTopVertical(targetWindow, HORIZONTAL_SCREEN_WIDTH, HORIZONTAL_SCREEN_HEIGHT);
-        }
-
-        private bool ResizeWindowCenterMaximizedHorizontal(Programs targetWindow)
-        {
-            return WindowCommands.ResizeWindowCenterMaximizedHorizontal(targetWindow);
-        }
-
-        private bool ResizeWindowRightHalfBottomVertical(Programs targetWindow)
-        {
-            return WindowCommands.ResizeWindowRightHalfBottomVertical(targetWindow, HORIZONTAL_SCREEN_WIDTH, HORIZONTAL_SCREEN_HEIGHT);
-        }
-
-        private bool ResizeWindowLeftMaximizedVertical(Programs targetWindow)
-        {
-            return WindowCommands.ResizeWindowLeftMaximizedVertical(targetWindow);
-        }
-
         private void UpdateStatus(string message, bool commandUpdate = false, bool success = false)
         {
             Dispatcher.Invoke(() =>
@@ -299,17 +187,6 @@ namespace DevTools
                 UpdateStatus(appName + " failed to launch.", true, false);
         }
 
-        private void ResizeApp(Programs program, Func<Programs, bool> ResizeFunction)
-        {
-            var appName = ProgramsTool.GetDescription(program);
-            UpdateStatus("Resizing " + appName + "...");
-            var resizeStatus = ResizeFunction(program);
-            if (resizeStatus)
-                UpdateStatus(appName + " resized.", true, true);
-            else
-                UpdateStatus(appName + " failed to resize.", true, false);
-        }
-
         private void OnCreatePrClick(object sender, RoutedEventArgs e)
         {
             PrGenerator prGenerator = new PrGenerator();
@@ -324,15 +201,10 @@ namespace DevTools
             var chromeCheckBox = ChromeCheckBox.IsChecked.Value;
             var spotifyCheckBox = SpotifyCheckBox.IsChecked.Value;
             var terminalCheckBox = TerminalCheckBox.IsChecked.Value;
-            var vpnCheckBox = VpnCheckBox.IsChecked.Value;
             var vsCode = VSCodeCheckBox.IsChecked.Value;
             new Thread(() =>
             {
                 UpdateStatus("Launching programs...");
-
-                // Connect to vpn
-                if (vpnCheckBox)
-                    LaunchApp(Programs.VPN, ConnectToVpn);
 
                 // Launch slack
                 if (slackCheckBox)
@@ -361,21 +233,6 @@ namespace DevTools
                 // Delay and wait for programs to launch then resize
                 Thread.Sleep(1000);
 
-                // Resizing Slack window
-                if (slackCheckBox)
-                    ResizeApp(Programs.Slack, ResizeWindowRightHalfTopVertical);
-
-                // Resizing Outlook window
-                if (outlookCheckBox)
-                    ResizeApp(Programs.Outlook, ResizeWindowRightHalfBottomVertical);
-
-                // Resizing Chrome window
-                if (chromeCheckBox)
-                    ResizeApp(Programs.Chrome, ResizeWindowCenterMaximizedHorizontal);
-
-                // Resizing Spotify window
-                if (spotifyCheckBox)
-                    ResizeApp(Programs.Spotify, ResizeWindowLeftMaximizedVertical);
             }).Start();
         }
 
@@ -392,75 +249,6 @@ namespace DevTools
                 UpdateStatus("Failed to delete Temporary ASP.NET Files.", true, false);
                 Debug.WriteLine("Error: " + ex);
             }
-        }
-
-        private void OnFixWindowsClick(object sender, RoutedEventArgs e)
-        {
-            return;
-            var slackCheckBox = SlackCheckBox.IsChecked.Value;
-            var outlookCheckBox = OutlookCheckBox.IsChecked.Value;
-            var chromeCheckBox = ChromeCheckBox.IsChecked.Value;
-            var spotifyCheckBox = SpotifyCheckBox.IsChecked.Value;
-            new Thread(() =>
-            {
-                // Resizing Slack window
-                if (slackCheckBox)
-                    ResizeApp(Programs.Slack, ResizeWindowRightHalfTopVertical);
-                
-                // Resizing Outlook window
-                if (outlookCheckBox)
-                    ResizeApp(Programs.Outlook, ResizeWindowRightHalfBottomVertical);
-
-                // Resizing Chrome window
-                if (chromeCheckBox)
-                    ResizeApp(Programs.Chrome, ResizeWindowCenterMaximizedHorizontal);
-
-                // Resizing Spotify window
-                if (spotifyCheckBox)
-                    ResizeApp(Programs.Spotify, ResizeWindowLeftMaximizedVertical);
-            }).Start();
-        }
-
-        private DayOfWeek GetDayOfWeekFromDate(DateTime currentDate, int targetDate) {
-            return new DateTime(currentDate.Year, currentDate.Month, targetDate).DayOfWeek;
-        }
-
-        private void CheckPayDay() {
-            var currentDate = DateTime.Now;
-            var numDays = DateTime.DaysInMonth(currentDate.Year, currentDate.Month);
-            
-            var biWeeklySundayPayDay = GetDayOfWeekFromDate(currentDate, (numDays / 2) - 2);
-            var biWeeklySaturdayPayDay = GetDayOfWeekFromDate(currentDate, (numDays / 2) - 1);
-            var biWeeklyWeekDayPayDay = GetDayOfWeekFromDate(currentDate, numDays / 2);
-            
-            if ((biWeeklySundayPayDay == DayOfWeek.Friday) || 
-                (biWeeklySaturdayPayDay == DayOfWeek.Friday) || 
-                (biWeeklyWeekDayPayDay != DayOfWeek.Saturday || biWeeklyWeekDayPayDay != DayOfWeek.Sunday)) {
-                MessageBox.Show("It's halfway through the month today so it's pay day today!");
-                return;
-            }
-            
-            var endOfMonthSundayPayDay = GetDayOfWeekFromDate(currentDate, (numDays / 2) - 2);
-            var endOfMonthSaturdayPayDay = GetDayOfWeekFromDate(currentDate, (numDays / 2) - 1);
-            var endOfMonthWeekDayPayDay = GetDayOfWeekFromDate(currentDate, numDays / 2);
-
-            if ((endOfMonthSundayPayDay == DayOfWeek.Friday) || 
-                (endOfMonthSaturdayPayDay == DayOfWeek.Friday) || 
-                (endOfMonthWeekDayPayDay != DayOfWeek.Saturday || endOfMonthWeekDayPayDay != DayOfWeek.Sunday)) {
-                MessageBox.Show("It's end of the month today so it's pay day today!");
-                return;
-            }
-        }
-
-        private void OnHomeTimeClick(object sender, RoutedEventArgs e)
-        {
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
-            {
-                if (!CheckTimeSheetSubmission())
-                    return;
-            }
-
-            CheckPayDay();
         }
 
         private void OnIncognitoClick(object sender, RoutedEventArgs e)
@@ -483,6 +271,16 @@ namespace DevTools
             {
                 UpdateStatus("Failed to update config file.", true, false);
             }
+        }
+
+        private void OnUpdateUserPath(object sender, TextChangedEventArgs e)
+        {
+            UpdateUserPath();
+        }
+
+        private void OnUpdateConfigPath(object sender, TextChangedEventArgs e)
+        {
+            UpdateConfigPath();
         }
     }
 }
